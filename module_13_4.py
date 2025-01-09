3b9f45c3dba20d5ee3ec80431730f9a54747f826
Andrey <abw66@yandex.ru>
2025-01-09 19:18:05 +0300

    Update module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import asyncio
 
-api = "7524985453:AAG6ZSjprpdu6F6ppQrTq6Uf9NmXCPQDrUM"
+api = ""
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
