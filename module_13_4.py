92dd34502da625e15755b36e47381447e569e8df
Andrey <abw66@yandex.ru>
2025-01-10 13:09:26 +0300

    Update module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -44,11 +44,11 @@ async def send_calories(message, state):
 
 @dp.message_handler(commands=['start'])
 async def start(message):
-    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 @dp.message_handler()
 async def all_message(message):
-    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == "__main__":
